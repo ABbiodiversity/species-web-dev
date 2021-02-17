27c34475d75841daf9621c4ad57c1813d1e8efa1
Peter Solymos <psolymos@gmail.com>
2021-02-17 11:05:00 -0700

    NN plants

---
+++ pages/nnplants/NNplantRich/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <spp :blob="blob"></spp>
+</template>
+<script>
+export default {
+  head () {
+    return {
+      title: this.blob.display
+    }
+  },
+  data () {
+    return {
+      blob: {
+        id:"NNplantRich",
+        scientific:"",
+        common:"Non-native Plant Richness",
+        display:"Non-native Plant Richness",
+        det:false,
+        useavailnorth:false,
+        useavailsouth:false,
+        coefnorth:true,
+        coefsouth:true,
+        map:true,
+        sectornorth:false,
+        sectorsouth:false,
+        idprev:"NNplantRich",
+        idnext:"NNplantRich",
+        taxonid:"nnplants",
+        taxonname:"Non-native plant richness"
+      }
+
+    }
+  }
+}
+</script>
